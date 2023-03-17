234e333e0ba5b0c60714aca6253b2072e85723d0
giospes <giovannijacopo.spes@gmail.com>
2023-03-17 17:01:41 +0100

    squares generated

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function game(){
     const myForm = document.querySelector('form');
-
+    
     myForm.addEventListener('submit' , function(e){
         e.preventDefault();
         const difficoltà = document.querySelector('#Difficoltà').value;
-        console.log(difficoltà);
+        printSquares(difficoltà)
     })
 
 
@@ -12,5 +12,27 @@ function game(){
 
 
 }
+
+function printSquares(numSqr){
+    let square, squares = [];
+    const squareContainer = document.querySelector('.sqr-container')
+    for(let i = 1; i<=numSqr*numSqr; i++){
+        square = document.createElement("div");
+        square.innerHTML = `<span>${i}</span>`;
+        square.className = "border bg-primary d-flex align-items-center justify-content-center"
+        if(numSqr == 7){
+            square.classList.add("gs-hard")  
+        }else if(numSqr == 8){
+            square.classList.add("gs-medium")
+        }else{
+            square.classList.add("gs-easy")  
+        }
+        squares[i] = square
+    }
+
+    
+    squares.forEach(s => squareContainer.appendChild(s));
+}
+
 
 game();
